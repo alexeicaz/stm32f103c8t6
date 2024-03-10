d9008bf33ab705b2a79a79ddd092e255f3773c10
Alexei Cazacina <alexei.kazachina@yandex.com>
2024-03-10 16:34:18 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+use nb::block;
+
+use stm32f1xx_hal::{
+    prelude::*,
+    pac,
+    timer::Timer
+};
+use cortex_m_rt::entry;
+use embedded_hal::digital::v2::OutputPin;
+#[entry]
+fn fn main() -> ! {
+    let cp = cortex_m::Peripheralsls::take().unwarp();
+    let dp = pac::Peripherals::take().unwrap();
+
 }
+
